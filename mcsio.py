ab86e0481c6f10353b127c6afc06331374ab23d1
a0966959337 <a0966959337@gmail.com>
2018-11-29 14:14:13 +0800

    123

---
+++ mcsio.py
@@ -0,0 +1,65 @@
+import time
+import sys
+ import httplib, urllib
+import json
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+while True:
+	SwitchStatus = GPIO.input(24)
+	if( SwitchStatus == 0):
+		print('Button pressed')
+	else:
+		print('Button released')
+deviceId = "DtpOF1a1"
+deviceKey ="2RnmMW0h7alLa8E9" 
+def post_to_mcs(payload):
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey}
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect()
+			not_connected = 0
+		except (httplib.HTTPException, socket.error) as ex:
+			print "Error: %s" % ex 
+			# sleep 10 seconds
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers)
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
+	data = response.read()
+	conn.close()   
+ # Parse command line parameters.
+@@ -31,25 +51,30 @@
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+    sys.exit(1)
+ # Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+while 1:
+	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+ # Un-comment the line below to convert the temperature to Fahrenheit.
+# temperature = temperature * 9/5.0 + 32
+ # Note that sometimes you won't get a reading and
+# the results will be null (because Linux can't
+# guarantee the timing of calls to read the sensor).
+# If this happens try again!
+	if humidity is not None and temperature is not None:
+    		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+ 		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+			   {"dataChnId":"Temperature","values":{"value":t0}}]},{"dataChnID":"SwitchStatus","values":{"value":SwitchStatus}}]}
+		post_to_mcs(payload)
+		time.sleep(10)
+ 	else:
+    		print('Failed to get reading. Try again!')
+    		sys.exit(1)
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
+
